53c23e2b4e7a087ee5e7b8cf326ac350e442e1ed
Jack Spektor <jack.spektor@cucumber.co.nz>
2018-03-07 23:07:46 +1300

    Tests passing for number humanizer

--- NumberHumanizer.BusinessLogic/DecimalNumberHumanizer.cs
+++ NumberHumanizer.BusinessLogic/DecimalNumberHumanizer.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Humanizer;
 
 namespace NumberHumanizer.BusinessLogic
@@ -12,14 +9,40 @@ namespace NumberHumanizer.BusinessLogic
     {
         public string Transform(string number)
         {
+            if (string.IsNullOrEmpty(number))
+            {
+                throw new ArgumentNullException(nameof(number));
+            }
+
+            string[] numberParts = number.Split(new char[] {',','.'}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
             int numberValue = 0;
             if (!int.TryParse(number, out numberValue))
             {
-                throw new ArgumentException("Argument is not a number");
-
+                throw new ArgumentException("Argument is not a number", nameof(number));
             }
 
-            return numberValue.ToWords();
+            int decimalValue = 0;
+
+            if (numberParts.Length > 1)
+            {
+                if (!int.TryParse(number, out decimalValue))
+                {
+                    throw new ArgumentException("Argument is not a number", nameof(number));
+                }
+            }
+            
+            var result = new StringBuilder();
+            result.Append(numberValue.ToWords());
+            result.Append(numberValue != 1 ? " dollars" : " dollar");
+            if (decimalValue > 0)
+            {
+                result.Append(" and ");
+                result.Append(decimalValue.ToWords());
+                result.Append(decimalValue > 1 ? " cents" : " cent");
+            }
+
+            return result.ToString();
         }
     }
 }
