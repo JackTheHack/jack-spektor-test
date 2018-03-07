99e1be201381d75ace4bbb7aa3ee155623bff20d
Jack Spektor <jack.spektor@cucumber.co.nz>
2018-03-07 22:54:52 +1300

    Use Humanizer implementation for number transform

---
+++ NumberHumanizer.BusinessLogic/DecimalNumberHumanizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace NumberHumanizer.BusinessLogic
+{
+    public class DecimalNumberHumanizer : INumberHumanizer
+    {
+        public string Transform(string number)
+        {
+            int numberValue = 0;
+            if (!int.TryParse(number, out numberValue))
+            {
+                throw new ArgumentException("Argument is not a number");
+
+            }
+
+            return numberValue.ToWords();
+        }
+    }
+}
---
+++ NumberHumanizer.BusinessLogic/INumberHumanizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberHumanizer.BusinessLogic
+{
+    public interface INumberHumanizer
+    {
+        string Transform(string number);
+    }
+}
